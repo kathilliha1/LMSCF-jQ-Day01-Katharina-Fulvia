b9f02f6636d7900712827e3b028843a94aa3c2be
Kathi <katharina.hansi@gmx.at>
2020-02-10 10:50:12 +0100

    intermediate exercise

--- Basic Exercise/myscript.js
+++ Basic Exercise/myscript.js
@@ -9,30 +9,31 @@
 // $("*").css("background-color", "yellow");
 
 // all p elements
-$("p").css("background-color","green");
+// $("p").css("background-color","green");
 
-// the p element with an ID of "intro"
-$("#intro").css("background-color", "yellow");
+// // the p element with an ID of "intro"
+// $("#intro").css("background-color", "yellow");
 
 // all elements with a class of "note"
-
+// $(".note").css("background-color", "brown");
 
 // all DIV elements with a class of note
-
+// $("div.note").css("background-color", "orange");
 
 // all p elements in the article element
-
+// $("article p").css("background-color", "orange");
 
 // the last element in any section which is a p tag
-
+// $("p:last-of-type").css("background-color", "orange");
 
 // the first paragraph on the page
-
+// $("p:first").css("background-color", "orange");
 
 // all p elements which have a link
-
+// $("p a").css("background-color", "orange");
 
 // the second li within nested ul element
-
+// $("li ul li:nth-child(2)").css("background-color", "orange");
 
 // every second and third p element
+$("p:nth-of-type(2), p:nth-of-type(3)").css("background-color", "orange");
